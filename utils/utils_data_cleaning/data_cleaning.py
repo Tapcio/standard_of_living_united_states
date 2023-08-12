06a8208cac7d6bb968a905307b893ebcf302ea7c
Adam <112779331+Tapcio@users.noreply.github.com>
2023-08-12 12:09:01 +0100

    Add files via upload
    Added main utils folder and moved all util folders with their functions there

---
+++ utils/utils_data_cleaning/data_cleaning.py
@@ -0,0 +1,202 @@
+import pandas as pd
+
+LINK_LENGTH_FIVE = 5
+LINK_LENGTH_FOUR = 4
+LINK_LENGTH_THREE = 3
+
+US_STATES = {
+    "AL": "Alabama",
+    "AK": "Alaska",
+    "AZ": "Arizona",
+    "AR": "Arkansas",
+    "CA": "California",
+    "CO": "Colorado",
+    "CT": "Connecticut",
+    "DE": "Delaware",
+    "DC": "District Columbia",
+    "FL": "Florida",
+    "GA": "Georgia",
+    "HI": "Hawaii",
+    "ID": "Idaho",
+    "IL": "Illinois",
+    "IN": "Indiana",
+    "IA": "Iowa",
+    "KS": "Kansas",
+    "KY": "Kentucky",
+    "LA": "Louisiana",
+    "ME": "Maine",
+    "MD": "Maryland",
+    "MA": "Massachusetts",
+    "MI": "Michigan",
+    "MN": "Minnesota",
+    "MS": "Mississippi",
+    "MO": "Missouri",
+    "MT": "Montana",
+    "NE": "Nebraska",
+    "NV": "Nevada",
+    "NH": "New Hampshire",
+    "NJ": "New Jersey",
+    "NM": "New Mexico",
+    "NY": "New York",
+    "NC": "North Carolina",
+    "ND": "North Dakota",
+    "OH": "Ohio",
+    "OK": "Oklahoma",
+    "OR": "Oregon",
+    "PA": "Pennsylvania",
+    "RI": "Rhode Island",
+    "SC": "South Carolina",
+    "SD": "South Dakota",
+    "TN": "Tennessee",
+    "TX": "Texas",
+    "UT": "Utah",
+    "VT": "Vermont",
+    "VA": "Virginia",
+    "WA": "Washington",
+    "WV": "West Virginia",
+    "WI": "Wisconsin",
+    "WY": "Wyoming",
+}
+
+
+def remove_special_character_school_rating(school_rating: str) -> str:
+    """
+    Cleaning of unnecessary characters after scraping Niche.com
+    Attributes:
+        str: school_rating
+    """
+    return school_rating.replace("Â", "")
+
+
+def remove_special_character_nightlife_rating(nightlife_rating: str) -> str:
+    """
+    Cleaning of unnecessary characters after scraping Niche.com
+    Attributes:
+        str: nightlife_rating
+    """
+    return nightlife_rating.replace("gradeÂ", "").replace("minus", "-").replace(" ", "")
+
+
+def remove_special_character_families_rating(families_rating: str) -> str:
+    """
+    Cleaning of unnecessary characters after scraping Niche.com
+    Attributes:
+        str: families_rating
+    """
+    return families_rating.replace("gradeÂ", "").replace("minus", "-").replace(" ", "")
+
+
+def create_state_from_link(link: str) -> str:
+    """
+    Gets the state Abbreviation from the link
+    Attributes:
+        str: link
+    """
+    split_url = link.split("/")[-2]
+    return split_url[-2:].upper()
+
+
+def number_to_int(number: str) -> int:
+    """
+    Creates int from the string that contains commas or $ signs
+    Attributes:
+        str: number
+    """
+    try:
+        return int(number.replace(",", "").replace("$", ""))
+    except:
+        return "No data available"
+
+
+def add_missing_type_of_place(link: str) -> str:
+    """
+    Link has "/n/" part for Neighbourhoods, otherwise it will be Suburb.
+    Towns or Cities are captured correctly when scraping, so only creating
+    Neighbourhood or Suburb.
+    Attributes:
+        str: link
+    """
+    link_split_list = link.split("/")
+    if link_split_list[4] == "n":
+        place_split = link_split_list[5].split("-")
+        return (
+            f"Neighborhood in {place_split[-2].capitalize()}, {place_split[-1].upper()}"
+        )
+    elif link_split_list[4] != "n":
+        place_split = link_split_list[4].split("-")
+        return f"Suburb of {place_split[-2].capitalize()}, {place_split[-1].upper()}"
+    else:
+        return "no data"
+
+
+def change_state_abbreviation_to_name(abbreviation: str) -> str:
+    """
+    Replicates full State name using dictionary with state names
+        str: abbreviation
+    Attributes:
+        dict: US_STATES
+    """
+    return US_STATES[abbreviation]
+
+
+def remove_niche_prefix(link: str) -> str:
+    """
+    Removes the generic part of the link and returns only the dynamic one that is related to the place
+    Attributes:
+        str: link
+    """
+    prefix1 = "https://www.niche.com/places-to-live/"
+    prefix2 = "https://www.niche.com/places-to-live/n/"
+
+    if link.startswith(prefix2):
+        return link[len(prefix2) :][:-1]
+    else:
+        return link[len(prefix1) :][:-1]
+
+
+def create_rent_to_sell_value_ratio(places_dataframe: pd.DataFrame) -> pd.DataFrame:
+    """
+    Creates column with rent to sell ratio
+    Attributes:
+        DataFrame: places_dataframe
+    """
+    filtered_places = places_dataframe.copy()
+    filtered_places["median_home_value"] = pd.to_numeric(
+        filtered_places["median_home_value"], errors="coerce"
+    )
+    filtered_places["median_rent"] = pd.to_numeric(
+        filtered_places["median_rent"], errors="coerce"
+    )
+    filtered_places["rent_sell_value_ratio"] = (
+        filtered_places["median_rent"] / filtered_places["median_home_value"]
+    )
+
+    return filtered_places
+
+
+def add_name_with_state(link: str) -> str:
+    """
+    Returns name place based on the length of the link's dynamic part.
+    Attributes:
+        str: link
+    """
+    link_without_prefix = remove_niche_prefix(link)
+    place_name_split = link_without_prefix.split("-")
+
+    if len(place_name_split) == LINK_LENGTH_FIVE:
+        place = f"{place_name_split[0].capitalize()} {place_name_split[1].capitalize()}"
+        city_or_county = (
+            f"{place_name_split[2].capitalize()} {place_name_split[3].capitalize()}"
+        )
+        state = place_name_split[4].upper()
+    elif len(place_name_split) == LINK_LENGTH_FOUR:
+        place = f"{place_name_split[0].capitalize()} {place_name_split[1].capitalize()}"
+        city_or_county = place_name_split[2].capitalize()
+        state = place_name_split[3].upper()
+    elif len(place_name_split) == LINK_LENGTH_THREE:
+        place = place_name_split[0].capitalize()
+        city_or_county = place_name_split[1].capitalize()
+        state = place_name_split[2].upper()
+    else:
+        return f"{place_name_split[0].capitalize()}, {place_name_split[1].upper()}"
+    return f"{place}, {city_or_county}, {state}"
